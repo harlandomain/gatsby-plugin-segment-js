843bda40f34fcf295fcb65838bbedf26ec736d95
James Harding <jamesvitaly1993@gmail.com>
2019-05-13 22:09:49 +0100

    Refactored to enable conditional setting for analytics.load()
    To help people build functionality that will support the eu cookie law, this pull requests adds in a conditional
    setting for analytics.load(). loadOnRender = false in the config file will remove the analytics.load() from init script
    and enable us to call this somewhere else in the app. E.g after a web visitor accepts cookies

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -8,6 +8,7 @@ exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
     host = "https://cdn.segment.io",
     delayLoad,
     delayLoadTime,
+    loadOnRender
   } = pluginOptions;
 
   // ensures Segment write key is present
@@ -25,6 +26,10 @@ exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
   // if trackPage option is falsy (undefined or false), remove analytics.page(), else keep it in by default
   // NOTE: do not remove per https://github.com/benjaminhoffman/gatsby-plugin-segment-js/pull/18
   const includeTrackPage = !trackPage ? "" : "analytics.page();";
+
+  // Stops initial loading of scripts, this lets you build functionality for cookie settings to comply with EU cookie law.
+  //  window.analytics.load() can then be called manually in app
+  const includeLoadOnRender = !loadOnRender ? "" : `analytics.load('${writeKey}');`;
 
   // Segment's minified snippet (version 4.1.0)
   const snippet = `!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="${host}/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
@@ -62,7 +67,7 @@ exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
   // if `delayLoad` option is true, use the delayed loader
   const snippetToUse = `
       ${delayLoad ? delayedLoader : ""}
-      ${snippet}
+      ${includeLoadOnRender ? snippet : ""}
     `;
 
   // only render snippet if write key exists
